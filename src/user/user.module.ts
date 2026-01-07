8d81710df780bbe9829da31e9a6abdf3b3311e05
xiaohu-pro <hhlworkspace@qq.com>
2026-01-07 15:31:59 +0800

    append local and jwt auth

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,18 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entity/user.entity';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
-  providers: [UserService],
+  providers: [
+    UserService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    }
+  ],
   controllers: [UserController]
 })
 export class UserModule {}
